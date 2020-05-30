8ac52db00a02571b7230a84b8a12adef165fc90f
shenyicheng <shenyicheng@souche.com>
2020-05-30 16:16:44 +0800

    chore: 增加jest的ts配置

---
+++ src/playground/LinkedList.ts
@@ -0,0 +1,8 @@
+// export default class LinkedList {
+//   tag = true;
+// }
+
+// Place your playground code here.
+export const test = (num: number): number => {
+  return num
+}
---
+++ src/playground/__test__/LinkedList.test.ts
@@ -0,0 +1,6 @@
+describe('LinkedList', () => {
+  it('should perform playground tasks', () => {
+    // Place your playground tests here.
+    expect(2).toBe(2);
+  });
+});
