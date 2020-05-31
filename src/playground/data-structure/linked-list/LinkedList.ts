7332e206c5d3c62a8feb2a73e74fd39a682e710f
shenyicheng <shenyicheng@souche.com>
2020-05-31 17:45:08 +0800

    chore: 增加jest babel插件配置

---
+++ src/playground/data-structure/linked-list/LinkedList.ts
@@ -0,0 +1,11 @@
+export default class LinkedList {
+  // constructor(props: any) {
+  //   super(props)
+  // }
+  toString = (cb?: Function): string => {
+    return ''
+  }
+}
+
+const l = new LinkedList()
+console.log('------> l', l.toString());
